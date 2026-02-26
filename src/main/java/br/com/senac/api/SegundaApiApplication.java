816692c1d559a7352567ffb772e708da2022096c
YOHANNA ALEXSANDRA KREUSCH <yohanna.kreusch@alunos.sc.senac.br>
2026-02-26 19:54:46 -0300

    primeiro commit

---
+++ src/main/java/br/com/senac/api/SegundaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundaApiApplication.class, args);
+	}
+
+}
