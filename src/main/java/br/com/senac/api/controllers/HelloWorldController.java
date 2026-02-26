d8f0fe9faa5cd73e5cd88dbc415658fa612e4e8f
YOHANNA ALEXSANDRA KREUSCH <yohanna.kreusch@alunos.sc.senac.br>
2026-02-26 20:17:01 -0300

    add HelloWorldController

---
+++ src/main/java/br/com/senac/api/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package br.com.senac.api.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/helloWorld")
+public class HelloWorldController {
+    @GetMapping("/teste")
+    public ResponseEntity<String> teste(){
+        return ResponseEntity.ok("Olá Mundo!");
+    }
+
+}
